2b21e0bb4c7540839291bce0b00c9213d3155110
mcosta21 <marcioc424@gmail.com>
2022-02-13 19:16:56 -0300

    Removing api-key

--- src/services/themoviedb/tvshow.api.ts
+++ src/services/themoviedb/tvshow.api.ts
@@ -7,7 +7,6 @@ const root = '/discover/tv';
 export async function getMovies(): Promise<PageableTheMovieDb<TVShow>> {
 
     const params = {
-        api_key: 'b9183c5c2d36811f09ae74cc21f99e94',
         with_watch_providers: 384,
         watch_region: 'BR',
         page: 1
@@ -20,7 +19,6 @@ export async function getMovies(): Promise<PageableTheMovieDb<TVShow>> {
 export async function getPopularTVShows(): Promise<PageableTheMovieDb<TVShow>> {
 
     const params = {
-        api_key: 'b9183c5c2d36811f09ae74cc21f99e94',
         with_watch_providers: 384,
         watch_region: 'BR',
         sort_by: 'realease_date.desc',
